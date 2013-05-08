1300bf6d6c0e9305be54ad0ffb9d9dfa16283764
EAK <ashok.zg@gmail.com>
2013-05-07 22:10:12 -0400

    Just some cleanup

--- balloc/src/locator.cpp
+++ balloc/src/locator.cpp
@@ -32,174 +32,151 @@ static const char WINDOW[] = "Image window";
 
 class ImageConverter
 {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  Mat imageA, imageB;
-  tf::TransformListener tf_;
-  ros::Time imTime;
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+	Mat imageA, imageB;
+	tf::TransformListener tf_;
+	ros::Time imTime;
+
+	image_geometry::PinholeCameraModel model;
 	
-  image_geometry::PinholeCameraModel model;
-
-  bool flag;
-  static const double blob_area_absolute_min_ = 300;
-  static const double blob_area_absolute_max_ = 11000;
-  static const double blob_compactness_ = 5;
+	bool flag;
+	static const double blob_area_absolute_min_ = 300;
+	static const double blob_area_absolute_max_ = 11000;
+	static const double blob_compactness_ = 5;
 public:
-  ImageConverter()
-    : it_(nh_)
-  {
-	  ros::Rate loop_rate(0.2);
-	flag = false;
-    image_pub_ = it_.advertise("out", 1);
-    image_sub_ = it_.subscribe("/prosilica/image_rect_color", 1, &ImageConverter::imageCb, this);
-    ros::Subscriber info_sub_ = nh_.subscribe("/prosilica/camera_info", 1, &ImageConverter::infoCb, this);
-        
-
-    namedWindow(WINDOW);
-    while(flag == false)
-    {
-    	loop_rate.sleep();
-    	ros::spinOnce();
-    }
-
-    ImageConverter::locator();
-  }
-
-  ~ImageConverter()
-  {
-    destroyWindow(WINDOW);
-  }
-  
-  void infoCb(const sensor_msgs::CameraInfo& msg)
-  {
-	model.fromCameraInfo(msg);
-	//SHUTDOWN LATER #HACK
-  }
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-    imageA = cv_ptr->image;
-    imageB = imageA.clone();
-    flag = true;
-    
-    //image_pub_.publish(cv_ptr->toImageMsg());
-  }
-  
-  void locator()
-  {
-    	namedWindow("Tracking");
-	  int hMin, hMax, sMin, sMax, vMin, vMax,area_min;
-	      hMin = 0;
-	      //hMax = 124; // night values/???
-	      hMax = 255;
-	      //sMin = 95;
-	      sMin = 126;
-	      sMax = 255;
-	      //vMin = 139;
-	      vMin = 173;
-	      vMax = 255;
-	      area_min = 100;
-	      Mat smoothed, hsvImg, t_img;
-  	      createTrackbar("blob min area","Tracking" ,&area_min ,1000);
-	      createTrackbar("Hue Min", "Tracking", &hMin, 255);
-	      createTrackbar("Hue Max", "Tracking", &hMax, 255);
-	      createTrackbar("Sat Min", "Tracking", &sMin, 255);
-	      createTrackbar("Sat Max", "Tracking", &sMax, 255);
-	      createTrackbar("Val Min", "Tracking", &vMin, 255);
-	      createTrackbar("Val MaX", "Tracking", &vMax, 255);
-	      //inRange(hsv, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), bw);
-	while(ros::ok())
+	ImageConverter() : it_(nh_)
 	{
-		Mat source = imageB;
-		Mat copy = imageB.clone();
+		ros::Rate loop_rate(0.2);
+		flag = false;
+		image_pub_ = it_.advertise("out", 1);
+		image_sub_ = it_.subscribe("/prosilica/image_rect_color", 1, &ImageConverter::imageCb, this);
+		ros::Subscriber info_sub_ = nh_.subscribe("/prosilica/camera_info", 1, &ImageConverter::infoCb, this);
 
-		//smoothed.create(source.rows, source.cols, source.type());
-		//cvPyrMeanShiftFiltering(&source, &smoothed, 2.0, 40.0);
-		GaussianBlur(source, smoothed, Size(9,9), 4);
-		cvtColor(smoothed, hsvImg, CV_BGR2HSV);
-		//inRange(hsvImg,Scalar(0,140,80),Scalar(30,255,130),t_img);
-		inRange(hsvImg, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), t_img);
-		//floodFill(t_img,Point(0,0),Scalar(255,255,255));
-//		for(int i=0;i<255;i++)
-//		{
-//			inRange(hsvImg,Scalar(0,140,80),Scalar(30,255,130),t_img);
-//			imshow("edited", t_img);
-//			if(waitKey(50)!=-1)
-//					cout<<i<<endl;
-//		}
-		CBlobResult blob;
-		IplImage i_img = t_img;
-		//vector<Vec3f> circles;
-		//HoughCircles(t_img, circles, CV_HOUGH_GRADIENT, 2, 40, 80, 40);
-		blob = CBlobResult(&i_img,NULL,0);
-		int num_blobs = blob.GetNumBlobs();
-		//cout<< num_blobs <<endl;
-	    blob.Filter(blob, B_INCLUDE, CBlobGetArea(), B_INSIDE, area_min, blob_area_absolute_max_);
-	    //blob.Filter(blob, B_EXCLUDE, CBlobGetCompactness(), B_GREATER, blob_compactness_);
-	    num_blobs = blob.GetNumBlobs();
-	    //cout<< "filtered blobs " <<num_blobs <<endl;
 
-	    std::string reference_frame = "/virtual_table"; // Table frame at ball_radius above the actual table plane
+		namedWindow(WINDOW);
+		while(flag == false)
+		{
+			loop_rate.sleep();
+			ros::spinOnce();
+		}
 
-	    tf::StampedTransform transform;
-	       // ros::Time cam_time = image_msg->header.stamp; // use image stamp since we might be mocking cam info
-            /// @todo Make tf usage optional? Fall back to ball radius approach
-       	    tf_.waitForTransform(reference_frame, model.tfFrame(), ros::Time(0), ros::Duration(0.5));
-       	    tf_.lookupTransform(reference_frame, model.tfFrame(), ros::Time(0), transform);
-	   
-	    for(int i =0;i<num_blobs;i++)
-	    {
-	    	CBlob* bl = blob.GetBlob(i);
-	    	Point2d uv(CBlobGetXCenter()(*bl), CBlobGetYCenter()(*bl));
-		uv.y = bl->MinY() + (bl->MaxX() - bl->MinX()) * 0.5;
-	   	circle(copy,uv,50,Scalar(255,0,0),5);
-	    	//cout<<uv.x << "and" << uv.y <<endl;
-
-		cv::Point3d xyz;
-	        model.projectPixelTo3dRay(uv, xyz);
-		
-		// Intersect ray with plane in virtual table frame
-		//Origin of camera frame wrt virtual table frame
-		tf::Point P0 = transform.getOrigin();
-		//Point at end of unit ray wrt virtual table frame
-          	tf::Point P1 = transform * tf::Point(xyz.x, xyz.y, xyz.z);
-		// Origin of virtual table frame		
-		tf::Point V0 = tf::Point(0.0,0.0,0.0);
-		// normal to the table plane
-          	tf::Vector3 n(0, 0, 1);	
-	 	// finding scaling value
-		double scale = (n.dot(V0-P0))/(n.dot(P1-P0));
-		tf::Point ball_pos = P0 + (P1-P0)*scale;
-		cout <<ball_pos.x() << " " << ball_pos.y() << " " << ball_pos.z() <<endl;		
-	    }
-		imshow(WINDOW, copy);
-		waitKey(3);
-
-		imshow("edited", t_img);
-		waitKey(3);
-
-		
-		
-    	ros::spinOnce();
+		ImageConverter::locator();
 	}
-  }
+
+	~ImageConverter()
+	{
+		destroyWindow(WINDOW);
+	}
+  
+	void infoCb(const sensor_msgs::CameraInfo& msg)
+	{
+		model.fromCameraInfo(msg);
+		//SHUTDOWN LATER #HACK
+	}
+	void imageCb(const sensor_msgs::ImageConstPtr& msg)
+	{
+		cv_bridge::CvImagePtr cv_ptr;
+		try
+		{
+			cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+		}
+		catch (cv_bridge::Exception& e)
+		{
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+		imageA = cv_ptr->image;
+		imageB = imageA.clone();
+		flag = true;
+		//image_pub_.publish(cv_ptr->toImageMsg());
+	}
+  
+	void locator()
+	{
+		namedWindow("Tracking");
+		int hMin, hMax, sMin, sMax, vMin, vMax,area_min;
+		hMin = 0;
+		//hMax = 124; // night values/???
+		hMax = 255;
+		//sMin = 95;
+		sMin = 126;
+		sMax = 255;
+		//vMin = 139;
+		vMin = 173;
+		vMax = 255;
+		area_min = 100;
+		Mat smoothed, hsvImg, t_img;
+		createTrackbar("blob min area","Tracking" ,&area_min ,1000);
+		createTrackbar("Hue Min", "Tracking", &hMin, 255);
+		createTrackbar("Hue Max", "Tracking", &hMax, 255);
+		createTrackbar("Sat Min", "Tracking", &sMin, 255);
+		createTrackbar("Sat Max", "Tracking", &sMax, 255);
+		createTrackbar("Val Min", "Tracking", &vMin, 255);
+		createTrackbar("Val MaX", "Tracking", &vMax, 255);
+		while(ros::ok())
+		{
+			Mat source = imageB;
+			Mat copy = imageB.clone();
+			GaussianBlur(source, smoothed, Size(9,9), 4);
+			cvtColor(smoothed, hsvImg, CV_BGR2HSV);
+			inRange(hsvImg, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), t_img);
+
+			CBlobResult blob;
+			IplImage i_img = t_img;
+			blob = CBlobResult(&i_img,NULL,0);
+			int num_blobs = blob.GetNumBlobs();
+
+			blob.Filter(blob, B_INCLUDE, CBlobGetArea(), B_INSIDE, area_min, blob_area_absolute_max_);
+			num_blobs = blob.GetNumBlobs();
+
+			std::string reference_frame = "/virtual_table"; // Table frame at ball_radius above the actual table plane
+
+			tf::StampedTransform transform;
+			tf_.waitForTransform(reference_frame, model.tfFrame(), ros::Time(0), ros::Duration(0.5));
+			tf_.lookupTransform(reference_frame, model.tfFrame(), ros::Time(0), transform);
+
+			for(int i =0;i<num_blobs;i++)
+			{
+				CBlob* bl = blob.GetBlob(i);
+				Point2d uv(CBlobGetXCenter()(*bl), CBlobGetYCenter()(*bl));
+				uv.y = bl->MinY() + (bl->MaxX() - bl->MinX()) * 0.5;
+				circle(copy,uv,50,Scalar(255,0,0),5);
+
+				cv::Point3d xyz;
+				model.projectPixelTo3dRay(uv, xyz);
+		
+				// Intersect ray with plane in virtual table frame
+				//Origin of camera frame wrt virtual table frame
+				tf::Point P0 = transform.getOrigin();
+				//Point at end of unit ray wrt virtual table frame
+				tf::Point P1 = transform * tf::Point(xyz.x, xyz.y, xyz.z);
+				// Origin of virtual table frame
+				tf::Point V0 = tf::Point(0.0,0.0,0.0);
+				// normal to the table plane
+				tf::Vector3 n(0, 0, 1);
+				// finding scaling value
+				double scale = (n.dot(V0-P0))/(n.dot(P1-P0));
+				tf::Point ball_pos = P0 + (P1-P0)*scale;
+				cout <<ball_pos.x() << " " << ball_pos.y() << " " << ball_pos.z() <<endl;
+			}
+			imshow(WINDOW, copy);
+			waitKey(3);
+
+			imshow("edited", t_img);
+			waitKey(3);
+
+			ros::spinOnce();
+		}
+	}
 };
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+	ros::init(argc, argv, "image_converter");
+	ImageConverter ic;
+	ros::spin();
+	return 0;
 }
